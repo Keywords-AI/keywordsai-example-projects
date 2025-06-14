4579718fa033b5fb24c797623b84ed059d3d6ce6
Raymond Huang <raymond@keywordsai.co>
2025-06-13 17:31:31 -0700

    (init) example projects

---
+++ vercel_ai_next_openai/instrumentation.ts
@@ -0,0 +1,12 @@
+import { registerOTel } from "@vercel/otel";
+import { KeywordsAIExporter } from "@keywordsai/exporter-vercel";
+
+export function register() {
+  registerOTel({
+    serviceName: "next-app",
+    traceExporter: new KeywordsAIExporter({
+      apiKey: process.env.KEYWORDSAI_API_KEY,
+      baseUrl: process.env.KEYWORDSAI_BASE_URL,
+    }),
+  });
+}
