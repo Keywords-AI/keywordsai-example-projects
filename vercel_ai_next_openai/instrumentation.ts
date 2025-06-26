da615b59593a8efab9cc5f164b3280dcf27e707f
Raymond Huang <raymond@keywordsai.co>
2025-06-25 17:26:49 -0700

    env example update

--- vercel_ai_next_openai/instrumentation.ts
+++ vercel_ai_next_openai/instrumentation.ts
@@ -1,6 +1,7 @@
 import { registerOTel } from "@vercel/otel";
 import { KeywordsAIExporter } from "@keywordsai/exporter-vercel";
 
+console.log("registering instrumentation, base url", process.env.KEYWORDSAI_BASE_URL);
 export function register() {
   registerOTel({
     serviceName: "next-app",
